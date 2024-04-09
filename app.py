d9d6dd942094f0bf3d877e8d8763fccf838e7a8d
bruno <brunorocha.freitass@hotmail.com>
2024-04-08 22:04:17 -0300

    Adding Read routes and update users

--- app.py
+++ app.py
@@ -47,9 +47,8 @@ def logout():
     logout_user()
     return jsonify({"message": "Logout realizado com sucesso"})
 
-# Cadastrar novas usuários
-@app.route("/user", methods=["POST"])
-@login_required 
+# CRUD - Create
+@app.route("/user", methods=["POST"]) 
 def create_user():
     data = request.json
     username = data.get("username")
@@ -63,6 +62,33 @@ def create_user():
         
     return jsonify ({"message": "Dados inválidos"}), 400
 
+# CRUD - Read
+@app.route("/user/<int:id_user>", methods=["GET"])
+@login_required
+def read_user(id_user):
+    user = User.query.get(id_user)
+    
+    if user:
+        return {"username": user.username}
+    
+    return jsonify({"message": "Usuário não encontrado!"}), 404
+
+# CRUD - Update
+@app.route("/user/<int:id_user>", methods=["PUT"])
+@login_required
+def update_user(id_user):
+    data = request.json
+    user = User.query.get(id_user)
+    
+    if user and data.get("password") :
+        user.password = data.get("password")
+        db.session.commit()
+         
+        return jsonify({"message": f"Usuário '{id_user}' atualizado com sucesso!"})
+    
+    return jsonify({"message": "Usuário não encontrado!"}), 404
+
+    
 @app.route("/hello-world", methods=["GET"])
 def hello_word():
     return "Hello World"
